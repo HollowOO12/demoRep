99a620fd4457e6a4c8b033edb156b4c94863c828
HollowOO12 <hollowoo12@gmail.com>
2021-06-27 11:12:47 -0500

    Update demoNamesheet.cpp

--- demoNamesheet.cpp
+++ demoNamesheet.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-    std::cout << "My name is Osamudiamen Okundaye";
+    std::cout << "My name is Osamudiamen Okundaye LUG's VP" << std::endl;
     //Please print your name under this
     return 0;
 }
